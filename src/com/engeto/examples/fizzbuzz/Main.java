1fed3a9bf79ae7122ed917af914b1ebefb139064
Bořek Rýdl <BBBorek@seznam.cz>
2022-08-09 08:33:15 +0200

    Vypis cisel delitelne 3
    pridana podminka, pokud je cislo delitelne 3 beze zbytku, vypise fizz

--- src/com/engeto/examples/fizzbuzz/Main.java
+++ src/com/engeto/examples/fizzbuzz/Main.java
@@ -5,8 +5,16 @@ public class Main {
     public static void main(String[] args) {
 
         for (int i = 1; i < 101 ; i++) {
-            System.out.println(i);
 
+            if (i % 3 == 0){ //zbytek po dělení 3 je nula
+
+                System.out.println("Fizz");
+            } else
+
+
+
+
+            System.out.println(i);
 
         }
 
